2de6d7cce7eb9317c745fcb5ef9d02707c6a4391
icodelifee <mithunsmenon@gmail.com>
2022-04-24 15:50:53 +0530

    chore(ffa): rename views to features

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -4,7 +4,7 @@ import 'package:one_context/one_context.dart';
 import '../di.dart';
 import '../gen/fonts.gen.dart';
 import 'config/colors.dart';
-import 'views/homepage/pages/homepage.dart';
+import 'features/homepage/pages/homepage.dart';
 
 class App extends StatefulWidget {
   const App({Key? key}) : super(key: key);
