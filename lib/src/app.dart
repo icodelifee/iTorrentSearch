24395841082554d4c1e763208ca47dfa09bc5fb2
icodelifee <mithunsmenon@gmail.com>
2022-05-02 00:04:36 +0530

    feat: make App stateless widget

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -6,14 +6,9 @@ import '../gen/fonts.gen.dart';
 import 'config/colors.dart';
 import 'features/home/screens/homepage.dart';
 
-class App extends StatefulWidget {
+class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
 
-  @override
-  _AppState createState() => _AppState();
-}
-
-class _AppState extends State<App> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
