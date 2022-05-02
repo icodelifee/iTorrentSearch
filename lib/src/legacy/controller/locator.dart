f0bd702fcf69f9e53547c7d31ea89fad244130b9
icodelifee <mithunsmenon@gmail.com>
2022-05-02 15:27:21 +0530

    chore: remove legacy dir

--- lib/src/legacy/controller/locator.dart
+++
@@ -1,8 +0,0 @@
-// import 'package:get_it/get_it.dart';
-// import 'package:itorrentapp_rebase/controller/homepage.dart';
-
-// final getIt  = GetIt.I;
-
-// void setUpLocator(){
-//     getIt.registerSingleton(HomePageState(),signalsReady: true);
-// }
