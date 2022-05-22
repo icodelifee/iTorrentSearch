fe3aee87d0cb6fdd3a33d1c95f46b4bb80f0d129
icodelifee <mithunsmenon@gmail.com>
2022-05-22 18:51:27 +0530

    feat: use dot env to store secrets

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import 'di.dart';
 import 'src/app.dart';
 import 'src/config/colors.dart';
 
-void main() {
+void main() async {
   // InAppPurchaseConnection.enablePendingPurchases();
   WidgetsFlutterBinding.ensureInitialized();
 
-  SystemChrome.setPreferredOrientations([
+  await SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitDown,
     DeviceOrientation.portraitUp,
   ]);
+  await dotenv.load(fileName: '.env');
   SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
     systemNavigationBarColor: CColors.backgroundColor, // navigation bar color
     statusBarColor: CColors.backgroundColor, // status bar color
