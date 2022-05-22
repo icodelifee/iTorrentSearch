6e56e544e2bfc9a47a161f3a7977ccf97fb9e0e4
icodelifee <mithunsmenon@gmail.com>
2022-05-22 18:51:27 +0530

    feat: use dot env to store secrets

--- lib/di.dart
+++ lib/di.dart
@@ -1,10 +1,12 @@
+import 'dart:convert';
+
 import 'package:dio/dio.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get_it/get_it.dart';
 import 'package:injectable/injectable.dart';
 import 'package:one_context/one_context.dart';
 
 import 'di.config.dart';
-import 'src/config/constants.dart';
 
 final di = GetIt.instance;
 
@@ -14,10 +16,11 @@ void configureDependencies() => $initGetIt(di);
 @module
 abstract class RegisterModules {
   @Named('BaseUrl')
-  String get baseUrl => Constants.apiUrl;
+  String get baseUrl => dotenv.env['API_URL'] ?? '';
 
   @lazySingleton
-  Dio get dio => Dio()..options.headers['Authorization'] = Constants.auth;
+  Dio get dio =>
+      Dio()..options.headers['Authorization'] = "Basic ${base64Encode(utf8.encode(dotenv.env['API_AUTH'] ?? ''))}";
 
   @singleton
   OneContext get oneContext => OneContext();
